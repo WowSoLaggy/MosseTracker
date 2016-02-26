56859bf438ecc0876480621ebd5de669d77cb4a2
dohxehapo <dohxehapo@gmail.com>
2016-02-26 13:11:40 +0300

    - added two test video

--- MosseTest/startup.cpp
+++ MosseTest/startup.cpp
@@ -8,12 +8,11 @@
 int main()
 {
 	TestFromFile test;
-	test.Run("d:\\Work\\MosseTracker\\Video\\dog\\", 8, 133, 100, 64, 64);//*/
-	//test.Run("d:\\Work\\MosseTracker\\Video\\dog\\", 8, 153, 130, 16, 16);
-	//test.Run("d:\\Work\\MosseTracker\\Video\\simple\\", 1, 0, 0, 64, 64);
+	//TestOneFrame test;
 
-	/*TestOneFrame test;
-	test.Run("d:\\Work\\MosseTracker\\Video\\dog\\", 8, 133, 100, 64, 64);//*/
+	//test.Run("d:\\Work\\MosseTracker\\Video\\dog\\", 8, 133, 100, 64, 64);
+	//test.Run("d:\\Work\\MosseTracker\\Video\\surfing\\", 8, 23, 55, 64, 64);
+	test.Run("d:\\Work\\MosseTracker\\Video\\boris\\", 8, 493, 475, 64, 64);
 
     return 0;
 }
